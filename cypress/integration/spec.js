a9c916b30402b8fde59c5ec7f1cd67939977c10d
Eliott Vincent <dev@eliottvincent.com>
2021-08-12 09:17:08 +0200

    feat: Upgrade to Vue 3 (#44)
    BREAKING CHANGE:
    
    Require Vue 3 and Vue Router 4

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Does not do much!', () => {
-    cy.visit('http://localhost:4000')
+    cy.visit('http://localhost:3000')
     cy.wait(1000)
     cy.window().then(({ pages }) => {
       expect([...pages]).to.deep.equal([1, 2, 3])
